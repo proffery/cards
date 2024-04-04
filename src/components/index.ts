1b00ef1437dfa1ba049104df36ba27cdcd5bf703
Dmitry Shamko <proffery@gmail.com>
2024-04-04 18:44:52 +0300

    Tabs component with styles and stories is done.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Button } from './ui/button/button'
 export { Input } from './ui/input/input'
+export { TabSwitcher } from './ui/tabSwitcher/tabSwitcher'
 export { Typography } from './ui/typography/typography'
---
+++ src/components/ui/tabSwitcher/tabSwitcher.tsx
@@ -0,0 +1,42 @@
+import { Fragment } from 'react'
+
+import { Tab } from '@headlessui/react'
+
+import s from './tabSwitcher.module.scss'
+
+export const TabSwitcher = () => {
+  return (
+    <Tab.Group>
+      <Tab.List>
+        <Tab as={Fragment}>
+          {({ selected }) => (
+            <button className={`${s.tab} ${selected ? s.selected : s.default}`}>Tab1</button>
+          )}
+        </Tab>
+        <Tab as={Fragment}>
+          {({ selected }) => (
+            <button className={`${s.tab} ${selected ? s.selected : s.default}`}>Tab2</button>
+          )}
+        </Tab>
+        <Tab as={Fragment}>
+          {({ selected }) => (
+            <button className={`${s.tab} ${selected ? s.selected : s.default}`} disabled>
+              Tab3
+            </button>
+          )}
+        </Tab>
+        <Tab as={Fragment}>
+          {({ selected }) => (
+            <button className={`${s.tab} ${selected ? s.selected : s.default}`}>Tab</button>
+          )}
+        </Tab>
+      </Tab.List>
+      <Tab.Panels>
+        <Tab.Panel>Content 1</Tab.Panel>
+        <Tab.Panel>Content 2</Tab.Panel>
+        <Tab.Panel>Content 3</Tab.Panel>
+        <Tab.Panel>Content 4</Tab.Panel>
+      </Tab.Panels>
+    </Tab.Group>
+  )
+}
