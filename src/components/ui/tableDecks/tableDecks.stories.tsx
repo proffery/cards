d04f1b1b5e9d07673472b61fca8d0f9b88788b3d
Dmitry Shamko <proffery@gmail.com>
2024-04-05 15:01:55 +0300

    DecksTable without cover

--- src/components/ui/tableDecks/tableDecks.stories.tsx
+++ src/components/ui/tableDecks/tableDecks.stories.tsx
@@ -1,7 +1,67 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { TableDecks } from '@/components'
+import { Deck, OrderDirection, OrderField, TableDecks } from '@/components'
+const myDecksExample: Deck[] = [
+  {
+    author: {
+      id: '1',
+      name: 'John Doe',
+    },
+    cardsCount: 10,
+    cover: 'https://example.com/cover1.jpg',
+    created: '2021-01-01',
+    id: '1',
+    isPrivate: true,
+    name: 'Deck 1',
+    updated: '2022-01-01',
+    userId: 'user1',
+  },
+  {
+    author: {
+      id: '2',
+      name: 'Mr. Anderson',
+    },
+    cardsCount: 0,
+    cover: 'https://example.com/cover2.jpg',
+    created: '2022-01-01',
+    id: '2',
+    isPrivate: true,
+    name: 'Deck 2',
+    updated: '2022-01-01',
+    userId: 'user2',
+  },
+]
 
+const allDecksExemple = [
+  {
+    author: {
+      id: '3',
+      name: 'Bob Johnson',
+    },
+    cardsCount: 18,
+    cover: 'https://example.com/cover3.jpg',
+    created: '2022-01-01',
+    id: '3',
+    isPrivate: false,
+    name: 'Deck 3',
+    updated: '2023-01-01',
+    userId: 'user3',
+  },
+  {
+    author: {
+      id: '4',
+      name: 'Alice Williams',
+    },
+    cardsCount: 0,
+    cover: 'https://example.com/cover4.jpg',
+    created: '2022-01-01',
+    id: '4',
+    isPrivate: false,
+    name: 'Deck 4',
+    updated: '2024-04-04',
+    userId: 'user4',
+  },
+]
 const meta = {
   component: TableDecks,
   tags: ['autodocs'],
@@ -11,41 +71,42 @@ const meta = {
 export default meta
 
 type Story = StoryObj<typeof meta>
-const decksExample = [
-  {
-    author: {
-      id: '3',
-      name: 'Bob Johnson',
-    },
-    cardsCount: 18,
-    cover: 'https://example.com/cover3.jpg',
-    created: '2022-03-01',
-    id: '3',
-    isPrivate: false,
-    name: 'Deck 3',
-    rating: 4.2,
-    shots: 8,
-    updated: '2022-03-05',
-    userId: 'user3',
-  },
-  {
-    author: {
-      id: '4',
-      name: 'Alice Williams',
-    },
-    cardsCount: 12,
-    cover: 'https://example.com/cover4.jpg',
-    created: '2022-04-01',
-    id: '4',
-    isPrivate: true,
-    name: 'Deck 4',
-    rating: 3.1,
-    shots: 2,
-    updated: '2022-04-05',
-    userId: 'user4',
-  },
-]
 
-export const Table: Story = {
-  render: () => <TableDecks items={decksExample} />,
+export const TableWithMyDecks: Story = {
+  args: {
+    items: myDecksExample,
+    onDelete: itemId => {
+      alert(`Delete button with id:${itemId} is pushed!`)
+    },
+    onEdit: itemId => {
+      alert(`Delete button with id:${itemId} is pushed!`)
+    },
+    onPlay: itemId => {
+      alert(`Delete button with id:${itemId} is pushed!`)
+    },
+    onSort: (orderDirection: OrderDirection, orderField: OrderField) => {
+      alert(`Table must be sorted: Field name: ${orderField}, Direction: ${orderDirection}`)
+    },
+    orderDirection: 'asc',
+    orderField: 'name',
+  },
+}
+export const TableWithAllDecks: Story = {
+  args: {
+    items: allDecksExemple,
+    onDelete: itemId => {
+      alert(`Delete button with id:${itemId} is pushed!`)
+    },
+    onEdit: itemId => {
+      alert(`Delete button with id:${itemId} is pushed!`)
+    },
+    onPlay: itemId => {
+      alert(`Delete button with id:${itemId} is pushed!`)
+    },
+    onSort: (orderDirection: OrderDirection, orderField: OrderField) => {
+      alert(`Table must be sorted: Field name: ${orderField}, Direction: ${orderDirection}`)
+    },
+    orderDirection: 'asc',
+    orderField: 'name',
+  },
 }
--- src/components/ui/tableDecks/tableDecks.tsx
+++ src/components/ui/tableDecks/tableDecks.tsx
@@ -1,11 +1,20 @@
-import { Edit, Play, Trash } from '@/assets/icons'
+import { ArrowDropDown, ArrowDropUp, Edit, Play, Trash } from '@/assets/icons'
 import { Typography } from '@/components'
 
 import s from './tableDecks.module.scss'
 
-type Props = {
+type DecksTableProps = {
   items?: Deck[]
+  onDelete: (itemId: string) => void
+  onEdit: (itemId: string) => void
+  onPlay: (itemId: string) => void
+  onSort: (orderDirection: OrderDirection, orderField: OrderField) => void
+  orderDirection?: OrderDirection
+  orderField?: OrderField
 }
+export type OrderDirection = 'asc' | 'desc'
+export type OrderField = 'author.name' | 'cardsCount' | 'name' | 'updated'
+
 export type Author = {
   id: string
   name: string
@@ -18,41 +27,107 @@ export type Deck = {
   id: string
   isPrivate: boolean
   name: string
-  rating: number
-  shots: number
   updated: string
   userId: string
 }
-export const TableDecks = ({ items }: Props) => {
+export const TableDecks = ({
+  items,
+  onDelete,
+  onEdit,
+  onPlay,
+  onSort,
+  orderDirection = 'asc',
+  orderField = 'name',
+}: DecksTableProps) => {
+  const sortIconsSwitcher = (fieldName: OrderField) => {
+    if (fieldName === orderField) {
+      return orderDirection === 'asc' ? <ArrowDropDown /> : <ArrowDropUp />
+    }
+  }
+  const playButtonHandler = (itemId: string) => {
+    onPlay(itemId)
+  }
+  const editButtonHandler = (itemId: string) => {
+    onEdit(itemId)
+  }
+  const deleteButtonHandler = (itemId: string) => {
+    onDelete(itemId)
+  }
+  const sortButtonHandler = (orderField: OrderField) => {
+    onSort(orderDirection === 'asc' ? 'desc' : 'asc', orderField)
+  }
+
   return (
     <table className={s.table}>
-      <tr className={s.row}>
-        <Typography.Subtitle2 as={'th'}>Name</Typography.Subtitle2>
-        <Typography.Subtitle2 as={'th'}>Cards</Typography.Subtitle2>
-        <Typography.Subtitle2 as={'th'}>Last Updated</Typography.Subtitle2>
-        <Typography.Subtitle2 as={'th'}>Created by</Typography.Subtitle2>
-      </tr>
-      {items?.map(item => (
-        <tr className={s.row} key={item.id}>
-          <Typography.Body2 as={'td'}>{item.name}</Typography.Body2>
-          <Typography.Body2 as={'td'}>{item.cardsCount}</Typography.Body2>
-          <Typography.Body2 as={'td'}>{item.updated}</Typography.Body2>
-          <Typography.Body2 as={'td'} className={s.withButtons}>
-            <Typography.Body2 as={'span'}>{item.author.name}</Typography.Body2>
-            <div className={s.buttonsContainer}>
-              <button>
-                <Play size={16} />
-              </button>
-              <button>
-                <Edit size={16} />
-              </button>
-              <button>
-                <Trash size={16} />
-              </button>
-            </div>
-          </Typography.Body2>
+      <thead>
+        <tr className={s.row}>
+          <Typography.Subtitle2 as={'th'} className={s.headCell}>
+            <button className={s.columnButton} onClick={() => sortButtonHandler('name')}>
+              Name{sortIconsSwitcher('name')}
+            </button>
+          </Typography.Subtitle2>
+          <Typography.Subtitle2 as={'th'} className={s.headCell}>
+            <button className={s.columnButton} onClick={() => sortButtonHandler('cardsCount')}>
+              Cards{sortIconsSwitcher('cardsCount')}
+            </button>
+          </Typography.Subtitle2>
+          <Typography.Subtitle2 as={'th'} className={s.headCell}>
+            <button className={s.columnButton} onClick={() => sortButtonHandler('updated')}>
+              Last Updated{sortIconsSwitcher('updated')}
+            </button>
+          </Typography.Subtitle2>
+          <Typography.Subtitle2 as={'th'} className={s.headCell}>
+            <button className={s.columnButton} onClick={() => sortButtonHandler('author.name')}>
+              Created by{sortIconsSwitcher('author.name')}
+            </button>
+          </Typography.Subtitle2>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {items?.map(item => (
+          <tr className={s.row} key={item.id}>
+            <Typography.Body2 as={'td'} className={s.bodyCell}>
+              {item.name}
+            </Typography.Body2>
+            <Typography.Body2 as={'td'} className={s.bodyCell}>
+              {item.cardsCount}
+            </Typography.Body2>
+            <Typography.Body2 as={'td'} className={s.bodyCell}>
+              {item.updated}
+            </Typography.Body2>
+            <Typography.Body2 as={'td'} className={`${s.bodyCell} ${s.cellWithButtons}`}>
+              <Typography.Body2 as={'span'}>{item.author.name}</Typography.Body2>
+              <div className={s.buttonsContainer}>
+                <button
+                  className={s.actionButton}
+                  disabled={item.cardsCount === 0}
+                  onClick={() => playButtonHandler(item.id)}
+                >
+                  <Play size={16} />
+                </button>
+                {item.isPrivate && (
+                  <button
+                    className={s.actionButton}
+                    disabled={item.cardsCount === 0}
+                    onClick={() => editButtonHandler(item.id)}
+                  >
+                    <Edit size={16} />
+                  </button>
+                )}
+                {item.isPrivate && (
+                  <button
+                    className={s.actionButton}
+                    disabled={item.cardsCount === 0}
+                    onClick={() => deleteButtonHandler(item.id)}
+                  >
+                    <Trash size={16} />
+                  </button>
+                )}
+              </div>
+            </Typography.Body2>
+          </tr>
+        ))}
+      </tbody>
     </table>
   )
 }
