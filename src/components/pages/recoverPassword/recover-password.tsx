96aed43577fba54b650d089477ed26dee02b66d1
xaosland <xaosland@mail.ru>
2024-04-23 18:30:50 +0400

    recover password page + correct link into sign-in component

---
+++ src/components/pages/recoverPassword/recover-password.tsx
@@ -0,0 +1,10 @@
+import { ForgotPassword } from '@/components/forms/forgot-password/forgot-password'
+import { Page } from '@/components/layouts'
+
+export const RecoverPassword = () => {
+  return (
+    <Page>
+      <ForgotPassword onSubmit={() => {}} />
+    </Page>
+  )
+}
