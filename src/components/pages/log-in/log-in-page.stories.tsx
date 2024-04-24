98b9dc99dc6492ffeb78475ab30bebb4fec5f5cb
Oleksii Rozhko <rozhkoaleks@gmail.com>
2024-04-24 16:54:10 +0300

    add login page

---
+++ src/components/pages/log-in/log-in-page.stories.tsx
@@ -0,0 +1,24 @@
+import { SignIn } from '@/components/forms/sign-in/sign-in'
+import { Layout, Page } from '@/components/layouts'
+import { Meta, StoryObj } from '@storybook/react'
+
+import { LogInPage } from './log-in-page'
+
+const meta = {
+  component: LogInPage,
+  tags: ['autodocs'],
+  title: 'Pages/LogInPage',
+} satisfies Meta<typeof LogInPage>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const LogInPageDefault: Story = {
+  render: () => (
+    <Layout>
+      <Page>
+        <SignIn onSubmit={() => {}} />
+      </Page>
+    </Layout>
+  ),
+}
