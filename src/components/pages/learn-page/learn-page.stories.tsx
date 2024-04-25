77422b70997d89f47a65c14ad1449de78c8395e4
xaosland <xaosland@mail.ru>
2024-04-25 20:53:02 +0400

    Learn Page + Radio Group for lern cards

---
+++ src/components/pages/learn-page/learn-page.stories.tsx
@@ -0,0 +1,38 @@
+import { Layout, Page } from '@/components/layouts'
+import { LearnPage } from '@/components/pages/learn-page/learn-page'
+import { Meta, StoryObj } from '@storybook/react'
+
+const meta = {
+  component: LearnPage,
+  tags: ['autodocs'],
+  title: 'Pages/LearnPage',
+} satisfies Meta<typeof LearnPage>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const LearnPageDefault: Story = {
+  render: () => (
+    <Layout>
+      <Page>
+        <LearnPage />
+      </Page>
+    </Layout>
+  ),
+}
+const imgForCard = {
+  answerImg: 'https://img.goodfon.ru/wallpaper/big/6/b8/verbena-tsvetochki-fioletovye.webp',
+  questionImg:
+    'https://img.goodfon.ru/wallpaper/big/f/99/tsvetok-seredinka-kolombina-lepestki.webp',
+}
+
+export const LearnPageWithPicturesDefault: Story = {
+  args: { card: imgForCard },
+  render: () => (
+    <Layout>
+      <Page>
+        <LearnPage card={imgForCard} />
+      </Page>
+    </Layout>
+  ),
+}
