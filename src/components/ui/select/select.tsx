18dbbe729c9bc8db1f129e7f9a8b7d0607a65aa1
Oleksii Rozhko <rozhkoaleks@gmail.com>
2024-04-28 18:42:01 +0300

    refactor select, add small variant for pagination

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -7,15 +7,20 @@ import clsx from 'clsx'
 
 import s from './select.module.scss'
 
+type VariantProps = {
+  variant?: 'small'
+}
+
 type SelectProps = {
   children?: ReactNode
   disabled?: boolean
   label?: string
   placeholder?: string
-} & ComponentPropsWithoutRef<typeof SelectRadix.Root>
+} & ComponentPropsWithoutRef<typeof SelectRadix.Root> &
+  VariantProps
 
 export const Select = (props: SelectProps) => {
-  const { children, disabled, label, placeholder, ...rest } = props
+  const { children, disabled, label, placeholder, variant, ...rest } = props
   const id = useId()
 
   return (
@@ -26,12 +31,20 @@ export const Select = (props: SelectProps) => {
         </Label>
       )}
       <SelectRadix.Root {...rest}>
-        <SelectRadix.Trigger aria-label={label} className={s.trigger} disabled={disabled} id={id}>
+        <SelectRadix.Trigger
+          aria-label={label}
+          className={clsx(s.trigger, variant === 'small' && s.smallTrigger)}
+          disabled={disabled}
+          id={id}
+        >
           <SelectRadix.Value placeholder={placeholder} />
           <ArrowDropDown className={s.icon} />
         </SelectRadix.Trigger>
         <SelectRadix.Portal>
-          <SelectRadix.Content className={s.content} position={'popper'}>
+          <SelectRadix.Content
+            className={clsx(s.content, variant === 'small' && s.smallContent)}
+            position={'popper'}
+          >
             <SelectRadix.Viewport>{children}</SelectRadix.Viewport>
           </SelectRadix.Content>
         </SelectRadix.Portal>
@@ -43,13 +56,18 @@ export const Select = (props: SelectProps) => {
 type SelectItemProps = {
   children?: ReactNode
   className?: string
-} & ComponentPropsWithoutRef<typeof SelectRadix.Item>
+} & ComponentPropsWithoutRef<typeof SelectRadix.Item> &
+  VariantProps
 
 export const SelectItem = forwardRef<HTMLDivElement, SelectItemProps>((props, ref) => {
-  const { children, className, ...rest } = props
+  const { children, className, variant, ...rest } = props
 
   return (
-    <SelectRadix.Item {...rest} className={clsx(s.item, className)} ref={ref}>
+    <SelectRadix.Item
+      {...rest}
+      className={clsx(s.item, variant === 'small' && s.smallItem)}
+      ref={ref}
+    >
       <SelectRadix.ItemText>{children}</SelectRadix.ItemText>
     </SelectRadix.Item>
   )
