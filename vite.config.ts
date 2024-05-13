433585e5657e867517b36c71ba64ca3e26ebbcb2
Dmitry Shamko <proffery@gmail.com>
2024-05-13 14:03:41 +0300

    remove favicon plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import * as path from 'path'
 
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-import { ViteFaviconsPlugin } from 'vite-plugin-favicon'
 
 export default defineConfig({
-  plugins: [react(), ViteFaviconsPlugin('src/assets/favicon/favicon.svg')],
+  plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
